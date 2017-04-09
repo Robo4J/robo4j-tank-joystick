0201b6bd4a918590d524c522126f5dcc0ddc6ea9
Miroslav Wengner <miro.wengner@gmail.com>
2017-04-09 20:41:09 +0200

    cleanup

--- src/main/java/com/robo4j/joystick/tank/controller/ImageController.java
+++ src/main/java/com/robo4j/joystick/tank/controller/ImageController.java
@@ -47,21 +47,18 @@ public class ImageController extends RoboUnit<CameraMessage> {
 
     @Override
     public void onMessage(CameraMessage message) {
-        SimpleLoggingUtil.print(getClass(), "onMessage target: "+ target + ", message: " + message );
         getContext().getReference(target).sendMessage(message);
     }
 
     @Override
     public void stop() {
         setState(LifecycleState.STOPPING);
-        System.out.println("Stopping server...");
         setState(LifecycleState.STOPPED);
     }
 
     @Override
     public void shutdown() {
         setState(LifecycleState.SHUTTING_DOWN);
-        System.out.println("shutting down example...");
         setState(LifecycleState.SHUTDOWN);
         System.exit(0);
     }
